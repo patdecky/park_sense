d3710c6455ad11fafb6b847af04bb702767eb103
patdecky <pat.decky@gmail.com>
2025-03-11 23:48:32 +0100

    Fix run script.

--- AI_backend/run_server.sh
+++ AI_backend/run_server.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Navigate to the directory where the server.py is located
-cd /home/deckyp/park_sense/AI_Backend
+cd /home/deckyp/park_sense/AI_backend
 
 # Activate the virtual environment
 source /home/deckyp/park_sense/venv/bin/activate
 
 # Run the Python server
-nohup python3 server.py > /home/deckyp/park_sense/AI_Backend/server.log 2>&1 &
+nohup python3 server.py > /home/deckyp/park_sense/AI_backend/server.log 2>&1 &
 
 # Get the Process ID (PID) and save it
-echo $! > /home/deckyp/park_sense/AI_Backend/server.pid
+echo $! > /home/deckyp/park_sense/AI_backend/server.pid
 
-echo "Server started with PID $(cat /home/deckyp/park_sense/AI_Backend/server.pid). Logs: /home/deckyp/park_sense/AI_Backend/server.log"
+echo "Server started with PID $(cat /home/deckyp/park_sense/AI_backend/server.pid). Logs: /home/deckyp/park_sense/AI_backend/server.log"
