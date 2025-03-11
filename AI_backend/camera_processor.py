3cdc31b9264827529c379aafd008fc02df5f947b
patdecky <pat.decky@gmail.com>
2025-03-11 21:41:15 +0100

    Added logging.

--- AI_backend/camera_processor.py
+++ AI_backend/camera_processor.py
@@ -4,16 +4,17 @@ from park_place_management import ParkingLotViewer, process_parking_lot
 from database_management import DatabaseMapper
 import datasource_windy
 import datasource_bezp_praha
+import logging
 
+log = logging.getLogger(__name__)
 
 
 
 def process_all_cameras(database_mapper:DatabaseMapper, pl_viewer:ParkingLotViewer):
     parking_lots = database_mapper.get_all_parking_lots_with_datasource()
-    availability = []
     for parking_lot in parking_lots:
+        log.debug(f"Processing parking lot {parking_lot.id}")
         datasources_for_parking_lot = database_mapper.get_data_sources_by_parkinglot(parking_lot.id)
-        
         datasource = datasources_for_parking_lot[0]
         match datasource.type:
             case 1:
@@ -21,17 +22,15 @@ def process_all_cameras(database_mapper:DatabaseMapper, pl_viewer:ParkingLotView
                 datasouce_object = datasource_windy.WindyDataSource(**datasource_data)
                 image = datasource_windy.read_live_image(datasouce_object.url)
             case 2:
-                print(datasource.source)
                 datasource_data = json.loads(datasource.source)
                 datasouce_object = datasource_bezp_praha.BEZPPrahaDataSource(**datasource_data)
                 image = datasource_bezp_praha.read_live_image(datasouce_object.url)
             case _:
-                print("Unknown datasource type")
+                log.error("Unknown datasource type")
 
         vacancy = process_parking_lot(image, parking_lot.id, parking_lot.car_capacity, pl_viewer)
-        availability.append(vacancy)
+        log.debug(f"Vacancy: {vacancy}")
         database_mapper.write_vacancy(parking_lot.id, vacancy)
-    return availability
 
 
 
--- AI_backend/server.py
+++ AI_backend/server.py
@@ -7,16 +7,60 @@ import cv2
 import matplotlib.pyplot as plt
 from camera_processor import process_all_cameras
 from park_place_management import ParkingLotViewer, convert_parking_lot_list, list_parking_places, calculate_availability, cars_in_parking_lots_iou_polygon
+import logging
+import logging.handlers
+import argparse
+import os
+
 
 import time
 
+parser = argparse.ArgumentParser(description="Park Sense - AI Backend")
+parser.add_argument('-debug', action='store_true', help="Enable debug logging")
+args, unknown = parser.parse_known_args()
+
+
+APP_NAME = "park_sense_ai_backend"
+LOG_DIR = "log"
+LOG_FILE = os.path.join(LOG_DIR, f"{APP_NAME}.log")
+
+os.makedirs(LOG_DIR, exist_ok=True)
+
+
+# Configure file handler
+file_handler = logging.handlers.TimedRotatingFileHandler(LOG_FILE, when="midnight", interval=1, backupCount=30, encoding="utf-8")
+file_formatter = logging.Formatter(
+    fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+file_handler.setFormatter(file_formatter)
+
+# Configure console handler
+console_handler = logging.StreamHandler()
+console_formatter = logging.Formatter(
+    fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+console_handler.setFormatter(console_formatter)
+
+# Configure logging
+log_level = logging.DEBUG if args.debug else logging.INFO
+
+
+# Set up the root logger
+root = logging.getLogger()
+root.setLevel(log_level)
+root.addHandler(file_handler)
+root.addHandler(console_handler)
+
 
 def process_all_data(database_mapper:DatabaseMapper, pl_viewer:ParkingLotViewer):
-    return process_all_cameras(database_mapper, pl_viewer)                
+    process_all_cameras(database_mapper, pl_viewer)                
 
 
 
 if __name__ == "__main__":
+
+    logging.info(f"Starting {APP_NAME}")
     
     config = ConfigNew(config_path="config.json")
 
@@ -29,12 +73,18 @@ if __name__ == "__main__":
     
     while True:
         try:
+            logging.info(f"Process start")
+            logging.debug("Connecting to database")
             database_mapper.connect()
-            print(process_all_data(database_mapper, pl_viewer))
+            logging.debug("Processing all data")
+            process_all_data(database_mapper, pl_viewer)
+        except Exception:
+            logging.exception(f"Server exception.", stack_info=True)
         except KeyboardInterrupt:
             break
         finally:
             database_mapper.disconnect()
+            logging.info(f"Process end")
         time.sleep(60)
 
 
