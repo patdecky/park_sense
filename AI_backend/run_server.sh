67204a4ee94e6dcaff0f6132935ba6fcee3a47fe
Patrik Decky <pat.decky@gmail.com>
2025-03-14 20:12:05 +0100

    upraven run script

--- AI_backend/run_server.sh
+++ AI_backend/run_server.sh
@@ -4,12 +4,13 @@
 cd /home/deckyp/park_sense/AI_backend
 
 # Activate the virtual environment
-source /home/deckyp/park_sense/venv/bin/activate
-
-# Run the Python server
-nohup python3 server.py > /home/deckyp/park_sense/AI_backend/server.log 2>&1 &
+source /home/deckyp/park_sense/AI_backend/venv/bin/activate
 
 # Get the Process ID (PID) and save it
 echo $! > /home/deckyp/park_sense/AI_backend/server.pid
 
 echo "Server started with PID $(cat /home/deckyp/park_sense/AI_backend/server.pid). Logs: /home/deckyp/park_sense/AI_backend/server.log"
+
+# Run the Python server
+python3 server.py > /home/deckyp/park_sense/AI_backend/server.log 2>&1 &
+
